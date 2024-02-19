91025ea84abab7f3ba69fd413aca322ffd678794
pmjherman <pmjherman@users.noreply.github.com>
2024-02-19 02:03:54 +0000

    Deploying to gh-pages from @ pmjherman/Seamly2D@3559a55cecac00a4c8c473aac8b885c11f621b9b 🚀

--- docs/search/classes_4.js
+++ docs/search/classes_4.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['editformuladialog_9157',['EditFormulaDialog',['../d1/d7d/classEditFormulaDialog.html',1,'']]],
-  ['editgroupdialog_9158',['EditGroupDialog',['../d3/da7/classEditGroupDialog.html',1,'']]],
-  ['editlabeltemplatedialog_9159',['EditLabelTemplateDialog',['../d7/df1/classEditLabelTemplateDialog.html',1,'']]],
-  ['expandingtextedit_9160',['ExpandingTextEdit',['../db/dac/classExpandingTextEdit.html',1,'']]],
-  ['exportformatcombobox_9161',['ExportFormatCombobox',['../d4/d06/classExportFormatCombobox.html',1,'']]],
-  ['exportlayoutdialog_9162',['ExportLayoutDialog',['../de/dbd/classExportLayoutDialog.html',1,'']]]
+  ['editformuladialog_9160',['EditFormulaDialog',['../d1/d7d/classEditFormulaDialog.html',1,'']]],
+  ['editgroupdialog_9161',['EditGroupDialog',['../d3/da7/classEditGroupDialog.html',1,'']]],
+  ['editlabeltemplatedialog_9162',['EditLabelTemplateDialog',['../d7/df1/classEditLabelTemplateDialog.html',1,'']]],
+  ['expandingtextedit_9163',['ExpandingTextEdit',['../db/dac/classExpandingTextEdit.html',1,'']]],
+  ['exportformatcombobox_9164',['ExportFormatCombobox',['../d4/d06/classExportFormatCombobox.html',1,'']]],
+  ['exportlayoutdialog_9165',['ExportLayoutDialog',['../de/dbd/classExportLayoutDialog.html',1,'']]]
 ];
--- docs/search/namespaces_2.js
+++ docs/search/namespaces_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['ui_9633',['Ui',['../db/d3c/namespaceUi.html',1,'']]],
-  ['utils_9634',['Utils',['../d4/d3d/namespaceUtils.html',1,'']]]
+  ['ui_9636',['Ui',['../db/d3c/namespaceUi.html',1,'']]],
+  ['utils_9637',['Utils',['../d4/d3d/namespaceUtils.html',1,'']]]
 ];
